4d32d7434b941ce4ef283c586bf72ea984d2a000
XiaoqingLee <xiaoqinglee9@gmail.com>
2018-09-16 11:45:05 +0800

    use html

--- app_dir/routes.py
+++ app_dir/routes.py
@@ -3,4 +3,16 @@ from app_dir import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world!"
+    user = {'username': "Xiaoqing"}
+    html = """
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, {} !<h1>
+        </body>
+    </html>
+        """
+    return html.format(user['username'])
+
