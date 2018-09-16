113a3b5769aa0040dfa158a284c3993ad41b10a1
XiaoqingLee <xiaoqinglee9@gmail.com>
2018-09-16 13:10:33 +0800

    制作表单对象，渲染到视图

--- app_dir/routes.py
+++ app_dir/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app_dir import app
+from app_dir.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,4 +18,11 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
 
