c6f8ef61f950779b8b92ccf5028406dad053a594
XiaoqingLee <xiaoqinglee9@gmail.com>
2018-09-16 18:02:35 +0800

    使用数据库，添加Flask shell上下文导出项

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # 静态类，不必实例化
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'who-will-know-what-am-i'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
--- microblog.py
+++ microblog.py
@@ -1,2 +1,9 @@
-from app_dir import app
+from app_dir import app, db
+from app_dir.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
 
