d44d9ed687bbe206c593eac7832bfee5a005ffe0
XiaoqingLee <xiaoqinglee9@gmail.com>
2018-09-17 22:58:32 +0800

    使用邮件错误警报

--- app_dir/__init__.py
+++ app_dir/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -14,6 +17,26 @@ login = LoginManager(app)
 # 函数内部，自动将用户重定向到login.login_view端点，同时在新请求的url后面
 # 添加？next=<url_for(原始请求的端点名用)>
 login.login_view = 'login'
+
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure =()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr=auth[0],
+            toaddrs=app.config['ADMINS'],
+            subject='Microblog Failure',
+            credentials=auth,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 
 from app_dir import routes, models, errors
